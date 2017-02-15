75810b61eef1c12b9719303c6f4428d81e4df345
gareth-rees-okta <gareth.rees@okta.com>
2017-02-15 15:23:21 +0000

    Update oktaconfig.js

--- oktaconfig.js
+++ oktaconfig.js
@@ -2,7 +2,7 @@ angular
 .module("OktaConfig", [])
 .constant('OKTACONFIG', {
 	baseUrl : "https://grdemo.okta.com/",
-	id: "DoeUFihrYt9yIfiswxQS",
+	id: "8tK4sFKAfIJk6tZULlQP",
 	redirect: "http://acmehealth-spa-oktaapi.44fs.preview.openshiftapps.com/",
 	authUrl : '/oauth2/aus9osgrgyEZ65aFs0x7/v1/authorize',
 	id_scopes: [
